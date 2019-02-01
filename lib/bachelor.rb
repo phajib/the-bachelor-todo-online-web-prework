51579c6af707ccde5867ca8788421e115972a3d6
H B <phajib@gmail.com>
2019-02-01 09:39:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,7 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |array|
     if array["status"] ==  "Winner"
-      returnarray["name"].split(" ").first
+      return array["name"].split(" ").first
     end
   end
 end
