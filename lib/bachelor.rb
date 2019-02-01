a3304c68c7af0ffe8ea4c1bd525ac2b6af7af06a
H B <phajib@gmail.com>
2019-02-01 10:08:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,7 +44,7 @@ def get_average_age_for_season(data, season)
   data.each do |season, array|
     array.each do |cont_data|
       count += 1
-      age += array["age"].to_f
+      age += array["age"].to_i
     end
   end
   (age/count).round
