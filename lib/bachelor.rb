7a7d555608de1c1e428de9c79119f216e365d396
H B <phajib@gmail.com>
2019-02-01 09:59:52 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,15 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |cont_data|
+      if cont_data["hometown"] == hometown
+        coutn += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
