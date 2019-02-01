4d8e1a691499aa9b0168c22b129b8c9d9984111c
H B <phajib@gmail.com>
2019-02-01 10:15:59 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,23 +1,53 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |array|
     if array["status"] ==  "Winner"
-      array["name"].split(" ").first
+      return array["name"].split(" ").first
     end
   end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |cont_data|
+      if cont_data["occupation"] == occupation
+        return cont_data["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season, array|
+    array.each do |cont_data|
+      if cont_data["hometown"] == hometown
+        count += 1
+      end
+    end
+  end
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, array|
+    array.each do |cont_data|
+      if cont_data["hometown"] == hometown
+        return cont_data["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age = 0
+  count = 0
+  avg = 0
+  data[season].each do |array|
+    array.each do |cont_data, values|
+      count += 1
+      age += array["age"].to_f
+    end
+    avg = (age/count).round
+  end
+  avg
 end
