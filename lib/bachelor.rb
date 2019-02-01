600950cb3bebdfbb6294af7f039f44940a782522
H B <phajib@gmail.com>
2019-02-01 10:00:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -21,7 +21,7 @@ def count_contestants_by_hometown(data, hometown)
   data.each do |season, array|
     array.each do |cont_data|
       if cont_data["hometown"] == hometown
-        coutn += 1
+        count += 1
       end
     end
   end
